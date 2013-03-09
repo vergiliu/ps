5072302741c51063e477522ab42e9ce9a2a2770c
vergiliu <vergiliu@gmail.com>
2013-03-09 09:05:11 +0100

    updated folder comparator, sync works, no folders

--- src/FolderComparator.py
+++ src/FolderComparator.py
@@ -2,12 +2,18 @@ import filecmp
 import os
 import sys
 import time
+import shutil
+
 # hash with all the files
 # initial root folder where all the files are coming of - we need 2 of these
+# folder support needs to be added
 
 
 class FolderComparatorStatus:
-    """test 543"""
+    """The class will store the last update of the FolderComparator and keep track
+    of the current status, for long running operations
+    """
+
     def __init__(self):
         self.lastRun = 0
         self.status = None
@@ -30,9 +36,12 @@ class FolderComparatorStatus:
 
 
 class FolderComparator:
-    """test 12345
+    """This class will be used to compare and sync 2 different folders
+    The folder structure will be considered as LEFT and RIGHT (from a diff view)
+    TODO more comments to be added
     """
     syncTypeList = {'keepboth': 0, 'keepbigger': 1, 'keepnewer': 2, }
+
     def __init__(self, leftFolder, rightFolder):
         self.syncType = 0 # default keepboth
         self.comparatorStatus = FolderComparatorStatus()
@@ -76,17 +85,55 @@ class FolderComparator:
         if aSyncType.__class__ == "".__class__ and aSyncType in self.syncTypeList.keys():
             self.syncType = self.syncTypeList[aSyncType]
 
+    def copyFromRightToLeft(self, myFile):
+        print("copy r->l")
+        shutil.copy2(self.rightFolder + myFile, self.leftFolder + myFile)
+
+    def copyFromLeftToRight(self, myFile):
+        print("copy l->r")
+        shutil.copy2(self.leftFolder + myFile, self.rightFolder + myFile)
+
     def syncNow(self):
         for myFile in self.inLeftButNotInRight:
-            pass #shutil.copy2(self.leftFolder + myFile, self.rightFolder + myFile)
+            shutil.copy2(self.leftFolder + myFile, self.rightFolder + myFile)
         for myFile in self.inRightButNotInLeft:
-            pass #shutil.copy2(self.rightFolder + myFile, self.leftFolder + myFile)
+            shutil.copy2(self.rightFolder + myFile, self.leftFolder + myFile)
 
+        mySyncMode = self.getSyncType()
         for myFile in self.inBothButDifferent:
-            pass #print("default keep both copies, use bigger file, use newer file")
+            if mySyncMode == 0:
+                print("default keep both copies")
+                mySplitPath = os.path.split(self.leftFolder + myFile)
+                myNewFile = os.path.join(mySplitPath[0], "c0py_" + mySplitPath[1])
+                shutil.move(self.leftFolder + myFile, myNewFile) # left.a >> left.new_a
+                self.copyFromRightToLeft(myFile) # right.a -> left.a
+                mySplitPath = os.path.split(self.rightFolder + myFile)
+                myNewFileRight = os.path.join(mySplitPath[0], "c0py_" + mySplitPath[1])
+                shutil.copy2(myNewFile, myNewFileRight) # left.new_a -> right.new_a
 
-        # vector_stanga = [self.leftFolder + f for f in self.inBothButDifferent]
-        # vector_dreapta = [self.rightFolder + f for f in self.inBothButDifferent]
+                # print("L new", myNewFile)
+                # print("L", self.leftFolder + myFile)
+                # print("R", self.rightFolder + myFile)
+
+            elif mySyncMode == 1:
+                print("use bigger file")
+                myLFStat = self.filesLeft[myFile]
+                myRFStat = self.filesRight[myFile]
+                if myLFStat.st_size < myRFStat.st_size:
+                    self.copyFromRightToLeft(myFile)
+                else:
+                    self.copyFromLeftToRight(myFile)
+            else:
+                myLFStat = self.filesLeft[myFile]
+                myRFStat = self.filesRight[myFile]
+                print("use newer file")
+                if myLFStat.st_mtime < myRFStat.stmtime:
+                    self.copyFromRightToLeft(myFile)
+                else:
+                    self.copyFromLeftToRight(myFile)
+
+            # vector_stanga = [self.leftFolder + f for f in self.inBothButDifferent]
+            # vector_dreapta = [self.rightFolder + f for f in self.inBothButDifferent]
 
             # spl = os.path.split(path)
             # os.path.join(spl[0], 'c0py_' + spl[1]) =>
@@ -104,7 +151,7 @@ class FolderComparator:
         self.inRightButNotInLeft = set_b.difference(set_a)
         self.inBothButDifferent = set_a.intersection(set_b)
 
-        self.visitSimilarItems()
+        self.checkSimilarItems()
 
         self.comparatorStatus.updateStatus("comparison done")
 
@@ -127,9 +174,9 @@ class FolderComparator:
         for myFile in self.inBothButDifferent:
             print("    is the same file %s ? %s" % (myFile, filecmp.cmp(self.leftFolder + myFile, self.rightFolder + myFile)))
 
-        # no working?
-        # filecmp.cmpfiles(vector_stanga, vector_dreapta, filesTestArray, shallow=False)
-        # print("simiar files %s" % filesTestArray)
+            # no working?
+            # filecmp.cmpfiles(vector_stanga, vector_dreapta, filesTestArray, shallow=False)
+            # print("simiar files %s" % filesTestArray)
 
     def isInSync(self):
         if len(self.inLeftButNotInRight) == 0 and len(self.inRightButNotInLeft) == 0:
@@ -145,7 +192,7 @@ class FolderComparator:
         print("left array  %d elements occupying %04d Bytes" % (len(self.filesLeft), sys.getsizeof(self.filesLeft)))
         print("right array %d elements occupying %04d Bytes" % (len(self.filesRight), sys.getsizeof(self.filesRight)))
 
-    def visitSimilarItems(self):
+    def checkSimilarItems(self):
         myMatchedFiles = []
         for myFile in self.inBothButDifferent:
             if filecmp.cmp(self.leftFolder + myFile, self.rightFolder + myFile):
@@ -168,11 +215,12 @@ if __name__ == "__main__":
         # print(obiectule.getSyncType())
 
         if not obiectule.isInSync():
-            obiectule.printStats()
+            #obiectule.printStats()
+            obiectule.printReport()
             obiectule.syncNow()
+            obiectule.runComparison()
             obiectule.printReport()
         else:
             print("a-ok")
     else:
         print("error parsing arguments")
-
